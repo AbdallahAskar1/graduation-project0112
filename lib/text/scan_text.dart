c8a74c270ed710acf9b4aa701586dbff11fbf593
mohamed_design <mooellmoo01126@gmail.com>
2023-03-01 11:18:16 +0200

    amendment code

--- lib/text/scan_text.dart
+++ lib/text/scan_text.dart
@@ -1,32 +1,20 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:my_scan_for_solution/text/regx_for_text.dart';
-
 import '../cubit/cubit.dart';
 import '../cubit/state.dart';
 import '../modules/answer/answer.dart';
 import '../style/icon_broken.dart';
-// import 'package:untitled1/cubit/cubit.dart';
-// import 'package:untitled1/cubit/states.dart';
-// import 'package:untitled1/modules/asnswer/answer.dart';
-// import 'package:untitled1/modules/rate/rate_screen.dart';
-// import 'package:untitled1/style/icon_broken.dart';
 
 class ScanText extends StatefulWidget {
   final String text;
-  ScanText({
-    required this.text
-  });
+  const ScanText({super.key, required this.text});
 
   @override
   State<ScanText> createState() => _ScanTextState();
 }
 
 class _ScanTextState extends State<ScanText> {
-
-  TextEditingController scannedContraller = new TextEditingController();
+  TextEditingController scannedContraller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -44,56 +32,52 @@ class _ScanTextState extends State<ScanText> {
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                icon: Icon(
-                    IconBroken.Arrow___Left_2
-                ),
+                icon: const Icon(IconBroken.Arrow___Left_2),
               ),
             ),
             body: Padding(
               padding: const EdgeInsets.all(30),
               child: SingleChildScrollView(
-                physics: BouncingScrollPhysics(),
+                physics: const BouncingScrollPhysics(),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Q: ',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Q: ',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
                           ),
-                          TextField(
-                            keyboardType: TextInputType.text,
-                            maxLines: null,
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.normal,
-                            ),
-                            controller: scannedContraller,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                            ),
-                            onSubmitted: (value) {
-                              //store text after change to transmitted to answer page
-                              print('text is : ${value}');
-                              scannedContraller.text = value;
-                            },
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.text,
+                          maxLines: null,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.normal,
                           ),
-                        ],
-                      ),
+                          controller: scannedContraller,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                          ),
+                          onSubmitted: (value) {
+                            //store text after change to transmitted to answer page
+                            'text is : $value';
+                            scannedContraller.text = value;
+                          },
+                        ),
+                      ],
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 20,
                     ),
                     Container(
                         width: double.infinity,
                         height: 60,
-                        decoration: BoxDecoration(
+                        decoration: const BoxDecoration(
                           color: Colors.blue,
                           borderRadius: BorderRadius.only(
                             topLeft: Radius.circular(12),
@@ -102,18 +86,19 @@ class _ScanTextState extends State<ScanText> {
                         ),
                         child: MaterialButton(
                           onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(
-                              builder: (context) =>
-                                  AnswerRate(text: scannedContraller.text,),));
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AnswerRate(
+                                    text: scannedContraller.text,
+                                  ),
+                                ));
                           },
-                          child: Text(
-                            'Show The Answer', style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20
+                          child: const Text(
+                            'Show The Answer',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
-                          ),
-                        )
-                    ),
+                        )),
                   ],
                 ),
               ),
